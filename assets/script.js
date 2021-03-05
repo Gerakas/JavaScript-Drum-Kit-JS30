11a961d9419f70d361cf74eeccc886ffef16dca6
Gerakas <andreasgerakianakis@gmail.com>
2021-03-05 23:27:45 +0000

    Strted on the functionality of the key buttons

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
+var button = document.querySelectorAll('.button');
+
+button.forEach(btn => {
+    btn.addEventListener('click', function() {
+        console.log(btn.attributes["data-key"]);
+    })
+});
 
